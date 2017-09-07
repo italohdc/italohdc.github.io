39264bc73e036c5af3ae5cdff6fff69b7f72d774
Italo Lelis <italohdc@gmail.com>
2017-09-07 01:50:02 -0300

    testing toTop.js

---
+++ js/toTop.js
@@ -0,0 +1,7 @@
+$("#toTop").click(function () {
+   //1 second of animation time
+   //html works for FFX but not Chrome
+   //body works for Chrome but not FFX
+   //This strange selector seems to work universally
+   $("html, body").animate({scrollTop: 0}, 500);
+});
